8304ad6450c7b0270e615fd5be0220c6f3d39aed
isalazarch <isalazar90100@ufide.ac.cr>
2023-09-18 20:53:26 -0600

    Update de Isaac 2
    Update de Isaac 2

--- mi-archivo.js
+++ mi-archivo.js
@@ -2,4 +2,4 @@ Alert("Hola mundo_editado");
 //Gist link: https://gist.github.com/isalazarch/e6f607595c04300fb616e7f162f7db72
 //Project link: https://github.com/users/isalazarch/projects/1/views/1
 
-//Este es el cambio agregado por Isaac
+//Este es el cambio agregado por Isaac 2
